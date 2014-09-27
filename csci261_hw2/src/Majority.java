a04aa5be6e53fcbd1b99701a87a90b5018f2f4c6
Derek Brown <djb3718@rit.edu>
2014-09-26 22:12:40 -0400

    Completed functionality for finding n/2 majority.  Still working on n/3 majority algorithm.

---
+++ csci261_hw2/src/Majority.java
@@ -0,0 +1,219 @@
+/**
+ * Majority.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ * 
+ * Purpose	Given the size of an array of integers, and a listing of integers
+ * 			first determine whether a value consists of the majority of of the
+ * 			numbers, then determine whether there exists a value that makes up
+ * 			a third of the list.
+ *
+ */
+
+import java.util.Scanner;
+
+public class Majority {
+	
+	// Attributes
+	
+	private int numberOfValues;
+	private int[] theValues;
+	
+	// Constructor
+	
+	/**
+	 * Constructor for creating an instance of Majority
+	 * 
+	 * @param numVals	The number of values the data set holds
+	 * @param values	The data set of values
+	 */
+	public Majority( int numVals, int[] values ) {
+		this.numberOfValues = numVals;
+		this.theValues = values;
+	}//end Majority constructor
+	
+	// Methods
+	
+	/**
+	 * Creates a smaller array containing all of the values less than the
+	 * pivot
+	 * 
+	 * @param values	The array of values
+	 * @param pivot		the value checking against
+	 * 
+	 * @return	The array of values less than the pivot
+	 */
+	public int[] createSmallerList( int[] values, int pivot ) {
+		int size = 0;
+		for( int i = 0 ; i < values.length ; i++ ) {
+			if( values[i] < pivot ) {
+				size++;
+			}//end if
+		}//end for
+		int[] smallerList = new int[size];
+		size = 0;
+		for( int i = 0 ; i < values.length ; i++ ) {
+			if( values[i] < pivot ) {
+				smallerList[size] = values[i];
+				size++;
+			}//end if
+		}//end for
+		return smallerList;
+	}//end createSmallerList
+	
+	/**
+	 * Creates an array containing all of the values greater than the pivot
+	 * 
+	 * @param values	The array of values
+	 * @param pivot		The value checking against
+	 * 
+	 * @return	The array of values greater than the pivot
+	 */
+	public int[] createBiggerList( int[] values, int pivot ) {
+		int size = 0;
+		for( int i = 0 ; i < values.length ; i++ ) {
+			if( values[i] > pivot ) {
+				size++;
+			}//end if
+		}//end for
+		int[] biggerList = new int[size];
+		size = 0;
+		for( int i = 0 ; i < values.length ; i++ ) {
+			if( values[i] > pivot ) {
+				biggerList[size] = values[i];
+				size++;
+			}//end if
+		}//end for
+		return biggerList;
+	}//end createBiggerList
+	
+	/**
+	 * Counts the number of times that the pivot occurs in the array
+	 * 
+	 * @param values	The array of values to test
+	 * @param pivot		The value we are counting
+	 * 
+	 * @return	The number of times pivot occurs in values
+	 */
+	public int countPivot( int[] values, int pivot ) {
+		int count = 0;
+		for( int i = 0 ; i < values.length ; i++ ) {
+			if( values[i] == pivot ) {
+				count += 1;
+			}//end if
+		}//end for
+		return count;
+	}//end countPivot
+	
+	/**
+	 * Recursive function that finds the median in an array of values.
+	 * 
+	 * @param values	The array of values.
+	 * @param k			The position the median would be if array was sorted.
+	 * 					k = (length of values)/2 rounded down.
+	 * 
+	 * @return	The median of values.
+	 */
+	public int findMedian( int[] values, int k ) {
+		int mid = (int) Math.floor(values.length/2);
+		int pivot = values[mid];
+		int[] smallerList = createSmallerList( values, pivot );
+		int[] biggerList = createBiggerList( values, pivot );
+		int count = countPivot( values, pivot );
+		int size = smallerList.length;
+		if( ( k >= size ) && ( k < ( size+count ) ) ) {
+			return pivot;
+		}//end if
+		else if( k < size ) {
+			return findMedian( smallerList, k);
+		}//end else if
+		else {
+			return findMedian( biggerList, k-size-count );
+		}//end else
+	}//end findMedian
+	
+	/**
+	 * Determines if there exists a value in values that occurs more than n/2
+	 * times.
+	 * 
+	 * @param values	The array of values.
+	 * @param median	The median of values.
+	 * 
+	 * @return	true, if a majority exists.
+	 * 			false, otherwise.
+	 */
+	public boolean isMajority( int[] values, int median ) {
+		int size = (int) Math.floor(values.length/2);
+		int[] smallerList = createSmallerList( values, median );
+		int[] biggerList = createBiggerList( values, median );
+		int num_median = countPivot( values, median );
+		if( num_median >= ( size+1 ) ) {
+			return true;
+		}//end if
+		boolean littleMajority = true;
+		int firstValue = smallerList[0];
+		for( int i = 1 ; i < smallerList.length ; i++ ) {
+			if( smallerList[i] != firstValue ) {
+				littleMajority = false;
+				break;
+			}//end if
+		}//end for
+		if( littleMajority ) {
+			if( firstValue != median ) {
+				littleMajority = false;
+			}//end if
+		}//end if
+		boolean bigMajority = true;
+		firstValue = biggerList[0];
+		for( int i = 1 ; i < biggerList.length ; i++ ) {
+			if( biggerList[i] != firstValue ) {
+				bigMajority = false;
+				break;
+			}//end if
+		}//end for
+		if( bigMajority ) {
+			if( firstValue != median ) {
+				bigMajority = false;
+			}//end if
+		}//end if
+		return (littleMajority || bigMajority );
+	}//end isMajority
+
+	/**
+	 * Reads in the data to be used,
+	 * Determines if there exists a value in the data set that occurs more
+	 * than n/2 times,
+	 * Determines if there exists a value in the data set that occurs more
+	 * than n/3 times,
+	 * Displays YES or NO for each depending on outcome.
+	 * 
+	 * @param args	not used.
+	 */
+	public static void main( String[] args ) {
+		
+		// Read in the values
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		int numVals = Integer.parseInt(input);
+		int[] values = new int[numVals];
+		for( int i = 0 ; i < numVals ; i++ ) {
+			input = sc.next();
+			int val = Integer.parseInt(input);
+			values[i] = val;
+		}//end for
+		sc.close();
+		Majority M = new Majority( numVals, values );
+		
+		// Find n/2 Majority
+		int median = M.findMedian(values, (int) Math.ceil( values.length/2) );
+		boolean halfMajority = M.isMajority( values, median );
+		if( halfMajority ) {
+			System.out.println("YES");
+		}//end if
+		else {
+			System.out.println("NO");
+		}//end else
+		
+		// Find n/3 Majority
+	}//end main
+}//end Majority
